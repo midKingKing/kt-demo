7444f13973db87eec2e05e6b351cbbfeba1f5530
jimi.feng <jimi.feng@alo7.com>
2020-07-28 21:47:06 +0800

    async await js style

--- src/main/kotlin/corotinue/Demo4.kt
+++ src/main/kotlin/corotinue/Demo4.kt
@@ -1,17 +1,32 @@
 package corotinue
 
-import kotlin.coroutines.Continuation
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
+import kotlinx.coroutines.asCoroutineDispatcher
+import test_pkg.log
+import java.util.concurrent.Executors
+import kotlin.coroutines.*
 
 interface AsyncScope
 
-fun async(block: suspend () -> Unit) {
-
+fun async(block: suspend AsyncScope.() -> Unit) {
+    val completion = AsyncCoroutine(Executors.newSingleThreadExecutor().asCoroutineDispatcher())
+    block.startCoroutine(completion, completion)
 }
 
 class AsyncCoroutine(override val context: CoroutineContext = EmptyCoroutineContext): Continuation<Unit>, AsyncScope {
     override fun resumeWith(result: Result<Unit>) {
         result.getOrThrow()
+    }
+}
+
+suspend fun <T> AsyncScope.await(block:() -> T) = suspendCoroutine<T> {
+    it.resume(block())
+}
+
+suspend fun main() {
+    //async await js style
+    async {
+        await {
+            log("in resume")
+        }
     }
 }
